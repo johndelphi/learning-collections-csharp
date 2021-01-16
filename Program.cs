1f85b30303c13dffe38cf7d5713296ab6ac73406
John.michael <John.michael@studentambassadors.com>
2021-01-15 23:38:16 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace learning_collections_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] firstname = { 1, 1, 2, 23, 3, 3, 3, 3, 3 };
+            //firstname[0] = 19;
+            //firstname[2] = 200;
+            //firstname[3] = 400;
+            //firstname[5] = 40000;
+            foreach (int item in firstname)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+    }
+}
